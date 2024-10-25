b8e4dfd6b72e260aebd85368b14c51739323dbdb
mrbravemanlabs <pwnishp.tech@gmail.com>
2024-10-25 09:24:46 +0545

    changed the database url

--- src/Database/database.js
+++ src/Database/database.js
@@ -3,9 +3,9 @@ import { databaseName } from "../constants.js";
 
 export async function connectDatabase() {
     console.log(process.env.DATABASE_URL);
-
+    const dbUrl = process.env.DATABASE_URL
     try {
-        const connectionInstance = await mongoose.connect(`mongodb://localhost:27017/${databaseName}`);
+        const connectionInstance = await mongoose.connect(`${dbUrl}/${databaseName}`);
 
         console.log("Database successfully connected.");
         console.log(`Host: ${connectionInstance.connection.host}`);
