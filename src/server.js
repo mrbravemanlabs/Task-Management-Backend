be8b8d465e2a46a2f81938147d47f62fc447bd26
mrbravemanlabs <pwnishp.tech@gmail.com>
2024-10-25 10:11:22 +0545

    changes in the port

--- src/server.js
+++ src/server.js
@@ -2,7 +2,7 @@ import app from "./app.js";
 import { connectDatabase } from "./Database/database.js";
 import dotenv from "dotenv";
 dotenv.config();
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 10000;
 connectDatabase()
     .then(() => {
         app.on("error", (error) => {
