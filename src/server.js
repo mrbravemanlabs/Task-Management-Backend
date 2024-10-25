dee3499a515aaa17735a99b25d502b1328c9effa
mrbravemanlabs <pwnishp.tech@gmail.com>
2024-10-25 10:02:15 +0545

    changed in the server codes

--- src/server.js
+++ src/server.js
@@ -1,26 +1,18 @@
 import app from "./app.js";
 import { connectDatabase } from "./Database/database.js";
 import dotenv from "dotenv";
-
 dotenv.config();
-
-const startServer = () => {
-    const port = process.env.PORT || 5000;
-
-    const server = app.listen(port, () => {
-        console.log(`App is successfully listening on port ${port}`);
-    });
-
-    server.on("error", (error) => {
-        console.error("Error occurred in the app:", error);
-        process.exit(1);
-    });
-};
-
+const port = process.env.PORT || 4000;
 connectDatabase()
     .then(() => {
-        console.log("Database connected successfully.");
-        startServer();
+        app.on("error", (error) => {
+            console.error("Error occurred in the app:", error);
+            process.exit(1);
+        })
+        app.listen(port , () => {
+            console.log(`App is successfully listening on port ${port}`)
+        })
+
     })
     .catch((error) => {
         console.error("Database connection failed:", error);
